163ed7d6b210b9b7009c6a17283598205eb27a50
Giovanni <10683631@polimi.it>
2023-03-18 11:03:34 +0100

    started to tinker with pattern in input file

--- pr.erl
+++ pr.erl
@@ -4,8 +4,8 @@
 get_input() ->
 	Read = string:chomp(io:get_line("Input file to process: ")),
 	{ok, File} = file:open(Read, [read, raw]),
-	{ok, Data} = read_file(File),
-	Data.
+	{ok, Op, Data} = read_pr_file(File),
+	[Op, Data].
 
 % Read file
 % Note: the input is parsed as a list of strings
@@ -16,9 +16,29 @@ read_file(File) ->
 read_file(File, Acc) ->
 	case file:read_line(File) of
 		{ok, Line} ->
-			read_file(File, [string:chomp(Line) | Acc]);
+			read_file(File, [string:trim(Line) | Acc]);
 		eof ->
 			file:close(File),
 			{ok, lists:reverse(Acc)}
 	end.
 
+% Read processing file
+read_pr_file(File) ->
+	read_pr_file(File, []).
+
+read_pr_file(File, []) ->
+	{ok, Op} = file:read_line(File),
+	[Module, Function | 	Args] = string:split(string:trim(Op), " ", all),
+	{ok, Tuple} = file:read_line(File),
+	{ok, Module, Function, Args, Tuple};
+	%{ok, Op, read_pr_file(File, [{K,V}])};
+
+
+read_pr_file(File, Acc) ->
+	case file:read_line(File) of
+		{ok, [K, " ", V]} ->
+			read_pr_file(File, [ {K, V} | Acc]);
+		eof ->
+			file:close(File),
+			lists:reverse(Acc)
+	end.
