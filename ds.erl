4e04d19b138a19f7ae900f1a5597b3797b044add
Nicolò <nicaschool13@gmail.com>
2023-03-18 19:01:55 +0100

    Dispatch work function + PDF informal documentation
    Dispatch work function WIP, partially implemented.
    From the available worker, will send the input and function and recollect the worker results.
    Added an informal documentation PDF for the functions and graphical visualization of the process spawning and control flow.

--- ds.erl
+++ ds.erl
@@ -1,5 +1,5 @@
 -module('ds').
--export([worker/0, worker/2, coordinator_start/0, coordinator_start/1, accept_connection/2, coordinator_listener/2, coordinator/2, dispach_work/2]).
+-export([worker/0, worker/2, coordinator_start/0, coordinator_start/1, accept_connection/2, coordinator_listener/2, coordinator/1, dispatch_work/1, get_result/2]).
 
 % Starts coordinator with default harcoded port 8080
 coordinator_start() ->
@@ -15,40 +15,43 @@ coordinator_start(Port) ->
         true -> 
             io:format("AcceptSocket generated, ready to listen for new workers~n"),
             % accepts incoming connections from the workers
-            Pid = spawn(?MODULE, coordinator, [[], []]),
+            Pid = spawn(?MODULE, coordinator, [[]]),
             accept_connection(Pid, AcceptSock)
             %spawn(?MODULE, accept_connection, [self(), AcceptSock])
     end.
     %coordinator([], []).
 
 % Coordinator has parameter R : ready workers (a list) and B: busy
-coordinator(R, B) ->
+coordinator(ReadyW) ->
     io:format("Coordinator waiting for workers"),
     receive
-        {S, join} -> 
-            io:format("accepted a join "),
-            L = [S | R]
-    after 1500 ->
-        L = R,
-        io:format("Ready processes: "),
-        io:fwrite("~w~n ",[length(L)]),
+        % Receives the socket of a new joining worker
+        {NewWSock, join} -> 
+            io:format("accepted a join ~n"),
+            NewReadyW = [NewWSock | ReadyW]
+    % After 2500 ms of no new joins, checks the number of workers
+    after 2500 ->
+        NewReadyW = ReadyW,
+        io:format("~nReady processes: "),
+        io:fwrite("~w~n",[length(NewReadyW)]),
         if 
-            length(L) == 0 ->
-                coordinator(R, B), 
+            length(NewReadyW) == length(ReadyW) ->
+                coordinator(ReadyW), 
                 halt();
             true -> 
                 io:format("Coordinator - TODO")
             end
     end,
-    Control = io:fread("~nCoordinator ready, do you want to start executing the tasks in the input file? [y/n]~n", "~s"),
+    {ok, Control} = io:fread("Coordinator ready, do you want to start executing the tasks in the input file? [y/n]", "~s"),
+    io:format(Control),    
     if 
-        Control == 'y' ->
-            %dispach_work(L, B);
-            Pid = spawn(?MODULE, dispach_work, [L, B]),
+        Control == ["y"] ->
+            io:format("~n Starting the normal executuion ~n"),
+            Pid = spawn(?MODULE, dispatch_work, [NewReadyW]),
             coordinator_loop(Pid),
             halt();
-        true -> 
-            coordinator(L, B), 
+        true ->
+            coordinator(NewReadyW),
             halt()
     end.
 
@@ -59,27 +62,69 @@ coordinator_loop(Scheduler) ->
 end,
 coordinator_loop(Scheduler).
 
-dispach_work(R, B) ->
-    if  % Case no busy jobs
-        B == [] ->
-        io:format("Dispatcher - TODO");
-            % TODO dispach the work and read the csv file 
-        % Case at least one worker is on
-        true ->
-            receive 
-                {Sock, result, R} ->
-                    io:format(R),
-                    io:format(Sock);
-                    % TODO
-                {Sock, error} ->
-                    io:format(Sock);
-                    % TODO
-                {Sock, join} ->
-                    io:format(Sock)
-                    % TODO
-            end
-    end.
-   
+
+% Send work takes the list of ready processes, the function to apply
+% and sends to all the ready processes, a lists of inputs (Input)
+% to be processed
+% While in execution, it changes the mapping between busy workers (sockets) 
+% and the submitted input to that node
+send_work(_, _ ,[], BusyWMap) ->
+    io:format("Send work empty"),
+    BusyWMap;
+send_work([ReadyW | RWList], Function, [Input | InList], BusyWMap) ->
+    io:format("Send work full"),
+    SendOutcome = gen_tcp:send(ReadyW, {work, Function, Input}),
+    case SendOutcome of 
+        ok -> 
+            NewBWMap = send_work(RWList, Function, InList, BusyWMap),
+            NewBWMap#{ReadyW => Input};
+        % In this case an error has occured
+        % TODO: it is possible to use a function to reunite the inputs
+        %       and divide them again to respect of the others Ready workers
+        true -> 
+            NewBWMap = send_work(RWList, Function, [Input | InList], BusyWMap)
+    end,
+    NewBWMap.
+
+%dispatch_work(ReadyWorker, []) ->
+%%    io:format("Dispatcher - TODO"),
+ %   % TODO the parameter will depend on the function and input data
+%    BusyWMap = send_work(ReadyWorker, [], [], #{}),
+%    dispatch_work(ReadyWorker, BusyWMap);
+%    % TODO dispach the work and read the csv file 
+
+% Case at least one worker is on
+dispatch_work(ReadyWorker) ->
+    EmptyMap = #{},
+    BusyWMap = send_work(ReadyWorker, [], [], EmptyMap),
+    get_result(BusyWMap, EmptyMap). 
+    
+get_result({}, ResultMap) ->
+    ResultMap;
+
+get_result(BusyWMap, ResultsMap) ->
+    receive 
+        {Sock, result, Result} ->
+            io:format(Result),
+            io:format(Sock),
+            NewResultMap = ResultsMap#{Sock => Result},
+            NewBusyMap = maps:remove(Sock, BusyWMap);
+            % TODO
+        {Sock, error} ->
+            NewBusyMap = BusyWMap,
+            NewResultMap = ResultsMap,
+            io:format(Sock);
+            % TODO
+        {Sock, join} ->
+            NewResultMap = ResultsMap,
+            NewBusyMap = BusyWMap,
+            io:format(Sock);
+        _ ->
+            NewResultMap = ResultsMap,
+            NewBusyMap = BusyWMap
+    end,
+    get_result(NewBusyMap, NewResultMap).
+            % TODO 
 
 % Accept connection accepts requests from other sockets.
 accept_connection(CoordPid, AcceptSock) ->
