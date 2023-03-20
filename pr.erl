91e41e4ba115586994e41cc304c8cb2244943360
Giovanni <10683631@polimi.it>
2023-03-20 07:31:26 +0100

    Using defines as God rules

--- pr.erl
+++ pr.erl
@@ -1,25 +1,6 @@
 -module(pr). %processing
 -export([get_input/0]).
-
--define(PARSE(TYPE, VAL, VAL_), 
-				case TYPE of 
-					int -> VAL_ = erlang:list_to_integer(VAL);
-					_ -> VAL_ = erlang:list_to_atom(VAL)
-				end).
-
--define(SPLIT(HEADER, OP, FUN, ARGS),
-				[OP_, FUN_, ARGS_] = string:split(string:trim(HEADER), " ", all),
-				OP = erlang:list_to_atom(OP_),
-				FUN = erlang:list_to_atom(FUN_),
-				case ARGS_ of
-					[] -> ARGS = [];
-					_ -> ARGS = erlang:list_to_integer(ARGS_)
-				end).
-
--define(SPLIT_(TYPES, K, V), 
-				[K_, V_] = string:split(string:trim(TYPES), " "),
-				K = erlang:list_to_atom(K_),
-				V = erlang:list_to_atom(V_)).
+-define(SPLIT(X), string:split(string:trim(X), " ", all)).
 
 % Coordinator function to get the file contating the input
 get_input() ->
@@ -32,18 +13,42 @@ get_input() ->
 %The second line contains the types of the variables
 read_file(File) ->
 	{ok, Header} = file:read_line(File),
-	?SPLIT(Header, Op, Fun, Args),
+	{Op, Fun, Args} = split_header(Header),
 	{ok, Types} = file:read_line(File),
-	?SPLIT_(Types, Key_type, Value_type),
+	{Key_type, Value_type} = split_types(Types),
 	{ok, Op, Fun, Args,
 	 read_file(File, file:read_line(File), Key_type, Value_type, [])}.
 
 read_file(File, {ok, Line}, Key_type, Value_type, Acc) ->
-	[Key, Value] = string:split(string:trim(Line), " "),
-	?PARSE(Key_type, Key, Key_),
-	?PARSE(Value_type, Value, Value_),
+	[Key, Value] = ?SPLIT(Line),
+	Key_ = parse(Key, Key_type),
+	Value_ = parse(Value, Value_type),
 	read_file(File, file:read_line(File), Key_type, Value_type, [{Key_, Value_} | Acc]);
 
 read_file(File, eof, _, _, Acc) ->
 	file:close(File),
 	lists:reverse(Acc).
+
+% UTILITY FUNCTIONS:
+% Split the types line into the types of the variables, converting them to atoms
+split_header(Header) ->
+	[Op_, Fun_, Args_] = ?SPLIT(Header),
+	{erlang:list_to_atom(Op_),
+	 erlang:list_to_atom(Fun_),
+	 case Args_ of
+	   [] -> [];
+		 _ -> erlang:list_to_integer(Args_)
+	 end}.
+
+split_types(Types) ->
+	[K_, V_] = ?SPLIT(Types),
+	{erlang:list_to_atom(K_),
+	 erlang:list_to_atom(V_)}.
+
+% Split the header line into the operations to be performed, the functions and eventually the arguments
+parse(V, V_type) ->
+	case V_type of 
+		int -> erlang:list_to_integer(V);
+		_ -> erlang:list_to_atom(V)
+	end.
+
