8c736b9092bd85ab2053e188db0488259efe201b
Nicolò <nicaschool13@gmail.com>
2023-03-22 13:48:34 +0100

    WIP on dispatch_work, send_work, get_result

--- src/coordinator.erl
+++ src/coordinator.erl
@@ -1,5 +1,5 @@
 -module('coordinator').
--export([start/0, start/1, accept_connection/3, coordinator/1, dispatch_work/1, get_result/3, socket_listener/2]).
+-export([start/0, start/1, accept_connection/3, coordinator/1, dispatch_work/1, socket_listener/2]).
 -importlib([file_processing, partition]).
 
 start() ->
@@ -62,22 +62,30 @@ coordinator(ReadyW) ->
 
 % Sends the work to the ready workers and waits for the results
 dispatch_work(Workers) ->
-    {ok, Op, Fun, Args, InputList} = file_processing:get_input(),
-    Work_force = lists:flatlength(Workers),
-    Inputs = partition:partition(InputList, Work_force),
+    {ok, Npartitions, [Op | Ops], InputList} = file_processing:get_input(),
+    Inputs = partition:partition(InputList, Npartitions),
     
     % TODO: add a Blocknumber to the input to be able to identify the block
-    InputsMap = map:from_list(lists:zip(lists:seq(1, Work_force), Inputs)),
+    InputsMap = map:from_list(lists:zip(lists:seq(1, Npartitions), Inputs)),
+    dispatch_work(Workers, [Op | Ops], InputsMap).  
 
+% Case no more operations
+% TODO : what to do with the workers? 
+% The dataflow program has ended, do we return them to 
+% use them for a next program?
+dispatch_work(_, [], Input) -> 
+    file_processing:save_data(Input)
+;
+
+dispatch_work(Workers, [Op | Ops], Input) ->
     % here should loop over the inputs and send the work to the workers
-    BusyWMap = send_work(Workers, {Op, Fun, Args},  InputsMap, #{}),
-
+    Send_work_Pid = spawn(?MODULE, send_work(Workers, Op, Input, self())),
+    NPartition = maps:size(Input),
     % Receives the ready workers for new work and the results from the get_result function 
-    io:fwrite("Waiting for results, busy map: ~w~n", [maps:to_list(BusyWMap)]),
-    [NewReadyW, ResultMap] = get_result([], BusyWMap, #{}, #{}),
+    [NewReadyW, ResultMap] = get_result(NPartition, Send_work_Pid),
     maps:values(ResultMap),
-    io:format(maps:to_list(ResultMap)),
-    dispatch_work(NewReadyW).
+    io:fwrite("~w~n", maps:to_list(ResultMap)),
+    dispatch_work(NewReadyW, Ops, ResultMap).
 
 % Send work takes the list of ready processes, the function to apply
 % and sends to all the ready processes, a lists of inputs (Input)
@@ -92,74 +100,88 @@ dispatch_work(Workers) ->
 % In this case, send_work what should do?
 % Notify the sending process was unsuccessful and give a non send input to be
 % reschedule when the receive routine gets the result back?
-send_work([], _ ,[], BusyWMap) ->
-    io:format("Send work empty ~n~n"),
-    BusyWMap;
 
-send_work([], _, Input, BusyWMap) ->
-    {error, {no_ready_workers, Input}, BusyWMap};
+% Case the input has been completely sended (empty list)
+send_work(_, _ ,[], _) ->
+    io:format("Work fully sended ~n~n");
 
-send_work([ReadyW | RWList], Function, [Input | InList], BusyWMap) ->
-    case gen_tcp:send(ReadyW, term_to_binary({work, Function, Input})) of 
+% Case no ready workers but still input to be dispatched
+% Waits for join from the get_result process
+send_work([], Op, Input, GetResultPid) ->
+    receive
+        {GetResultPid, join, Sock} ->
+            send_work([Sock], Op, Input, GetResultPid)
+    end;
+
+% Nominal case, There is still input to be sended and workers ready
+% to get new jobs
+send_work([ReadyW | RWList], Op, [Input | InList], GetResultPid) ->
+    case gen_tcp:send(ReadyW, term_to_binary({work, Op, Input})) of 
         ok -> 
             io:fwrite("Send successfully message~n"),
-            send_work(RWList, Function, InList, maps:put(ReadyW, Input, BusyWMap));
+            send_work(RWList, Op, InList, GetResultPid);
         {error, Error} ->
             io:fwrite("An error has occured sending the message: ~w~n", Error),
-            send_work(RWList, Function, [Input | InList], BusyWMap)
+            send_work(RWList, Op, [Input | InList], GetResultPid)
     end.
     
-% Get results, given the ReadyWorkers, BusyWorkerMap and the actual ResultMap, 
+% Get results, given the ReadyWorkers, BusyWorkerMap, the actual ResultMap and the send_work Pid
 % Returns in a list the updated ReadyWorkers and the updated ResultMap
 
-% Case no more result to wait
-get_result(ReadyW, {}, ResultMap) ->
-    [ReadyW, ResultMap].
+% Initialization of get result, with empty ready processes, empty busy and result map
+% and the send_work pid 
+get_result(NPartition, SenderPid) ->
+    get_result([], #{}, #{}, SenderPid, NPartition).
 
 % Case no Ready workers (waits to receive at least a worker)
-get_result([], {}, Result, _) ->
+get_result([], {}, Result, SenderPid, NPartition) ->
     io:format("Waiting for new ready workers to join, since no work are scheduled and no ready workers are available ~n"),
     receive
         {Sock, join} ->
-            [[Sock], #{}]
+            SenderPid ! {self(), join, Sock}
         %FIXME: Sock is ignored and lost
     end,
-    Result;
+    get_result([Sock], #{}, Result, SenderPid, NPartition);
 
 % Normal case
-get_result(ReadyW, BusyWMap, ResultsMap, SockOrderMap) ->
-    receive 
-        {Sock, result, Result} ->
+get_result(ReadyW, BusyWMap, ResultsMap, SenderPid, NPartition) ->
+    receive
+        % Get the result form a worker, update result map and send to 
+        % send_work that a worker now is newly available
+        {Sock, result, PartitionId, Result} ->
             io:fwrite("New results have arrived ~w ", [Result]),
             io:fwrite("from socket ~w~n", [Sock]),
-            #{Sock := Index} = SockOrderMap,
-            NewResultMap = ResultsMap#{Index => Result},
             NewBusyMap = maps:remove(Sock, BusyWMap),
-            NewReadyW = [Sock | ReadyW];
-            % TODO
+            NewReadyW = [Sock | ReadyW],
+            NewResultMap = maps:put(PartitionId, Result),
+            SenderPid ! {free, Sock};
+        % Case an error has occured, remove the sock
         {Sock, error} ->
             io:fwrite("Error from socket ~w~n", [Sock]),
             NewBusyMap = maps:remove(Sock, BusyWMap),
             NewResultMap = ResultsMap,
             %TODO - reschedule the job
-            NewReadyW = ReadyW
-            %BusyWUpToNow = maps:remove(Sock, BusyWMap),
-            %send_work(ReadyW, )
-            ;
+            NewReadyW = ReadyW;
             % TODO
         {Sock, join} ->
             io:fwrite("New process has join ~w~n", [Sock]),
             NewResultMap = ResultsMap,
             NewBusyMap = BusyWMap,
             NewReadyW = [Sock, ReadyW],
-            io:format(Sock);
+            io:format(Sock),
+            SenderPid ! {join, Sock};
         _ ->
             io:fwrite("Unrecognized message~n"),
             NewResultMap = ResultsMap,
             NewReadyW = ReadyW,
             NewBusyMap = BusyWMap
     end,
-    get_result(NewReadyW, NewBusyMap, NewResultMap).
+    case maps:size(NewResultMap) of
+     NPartition ->
+            NewResultMap;
+        true -> 
+            get_result(NewReadyW, NewBusyMap, NewResultMap, SenderPid, NPartition)
+    end.
 
 % Coordinator listener manages the communication with the worker
 % Waits to receive messages from the host and 
