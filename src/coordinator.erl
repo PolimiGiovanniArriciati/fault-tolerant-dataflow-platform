66493a2dd1bde4243832a9814ef4a53362084903
Giovanni <10683631@polimi.it>
2023-03-24 16:27:41 +0100

    Output input formatting

--- src/coordinator.erl
+++ src/coordinator.erl
@@ -1,9 +1,11 @@
 -module('coordinator').
 -export([start/0, start/1]).
 %Functions for internal use not called directly
--export([dispatch_work/3, get_results/3, accept_connection/3, socket_listener/2, jobs_queue/3, coordinator/2]).
+-export([dispatch_work/3, get_results/3, accept_connection/2, socket_listener/2, jobs_queue/4, coordinator/2]).
 -importlib([file_processing, partition]).
 -define(NAME, string:chomp(io:get_line("Input file to process: "))).
+-define(LOG(STRING), io:format("" ++ STRING ++ "~n")).
+-define(LOG(STRING, ARGS), io:format("" ++ STRING ++ "~n", ARGS)).
 
 start() ->
     start(8080).
@@ -12,42 +14,40 @@ start() ->
 start(Port) ->
     case gen_tcp:listen(Port, [binary, {packet, 0}, {active, false}]) of
         {ok, AcceptSock} -> 
-            io:format("AcceptSocket generated, ready to listen for new workers on port ~p~n", [Port]),
+            ?LOG("AcceptSocket generated, ready to listen for new workers on port ~p~n", [Port]),
             CoordinatorPid = spawn(?MODULE, coordinator, [[],[]]),
-            accept_connection(CoordinatorPid, AcceptSock, []);
+            accept_connection(CoordinatorPid, AcceptSock);
         {error, eaddrinuse} ->
-            io:format("Port ~p already in use, please choose another one~n", [Port]),
+            ?LOG("Port ~p already in use trying next one~n", [Port]),
             start(Port + 1);
         {error, ErrorMessage} -> 
-            io:format("Unexpected error launching the coordinator~n"),
-            io:format(ErrorMessage)
+            ?LOG("Unexpected error launching the coordinator: ~p~n", [ErrorMessage])
     end.
 
-accept_connection(Coordinator_id, AcceptSock, Workers) ->
+accept_connection(Coordinator_id, AcceptSock) ->
     case gen_tcp:accept(AcceptSock) of
         {ok, Sock} ->
-            io:format("Coordinator accepted a new connection~n"),
-            % Starts the specific worker Pid that waits to receive messages from the worker
-            Worker = spawn(?MODULE, socket_listener, [Coordinator_id, Sock]),
-            accept_connection(Coordinator_id, AcceptSock, Workers ++ [Worker]);
+            ?LOG("Coordinator accepted a new connection~n"),
+            spawn(?MODULE, socket_listener, [Coordinator_id, Sock]),
+            accept_connection(Coordinator_id, AcceptSock);
         {error, Error} ->
-            io:format("!!Unexpected error during socket accept!! ~w~n", Error),
-            accept_connection(Coordinator_id, AcceptSock, Workers)
+            ?LOG("Unexpected error during socket accept!! ~w~n", Error),
+            accept_connection(Coordinator_id, AcceptSock)
     end.
 
 % Coordinator has parameter R : ready workers (a list) and B: busy
 coordinator(Workers, JobsInProgress) ->
-    io:format("Coordinator waiting for workers"),
+    ?LOG("Coordinator waiting for workers"),
     receive
         % Receives the socket of a new joining worker
         {join, Worker} -> 
             Workers1 = [Worker | Workers]
     after 3000 ->
-            io:format("... no workers joined yet~n"),
+            ?LOG("no workers joined yet~n"),
             Workers1 = Workers
     end,
     if Workers1 == [] ->
-            io:format("No workers available, waiting for new workers to join~n"),
+            ?LOG("No workers available, waiting for new workers to join~n"),
             coordinator(Workers1, JobsInProgress);
         true ->
             case io:fread("Coordinator ready, do you want to start executing the tasks in the input file? [y/n]", "~s") of
@@ -56,18 +56,20 @@ coordinator(Workers, JobsInProgress) ->
                 {ok, ["n"]} ->
                     coordinator(Workers1, JobsInProgress);
                 {ok, _} ->
-                    io:format("Invalid input, please type 'y' or 'n'~n")
+                    ?LOG("Invalid input, please type 'y' or 'n'~n")
             end,
             coordinator(Workers1, [])
     end.
 
 start_work(Workers) ->
-    {ok, Npartitions, Ops} = file_processing:get_operations("in/op"), %FIXME:?NAME),
-    {ok, InputList}        = file_processing:get_data("in/data"),     %FIXME:?NAME),
+    Operations = get_in("Input operations to execute: "),
+    {ok, Npartitions, Ops} = file_processing:get_operations("in/" ++ Operations),
+    FileName =get_in("Input file to process: "),
+    {ok, InputList}        = file_processing:get_data("in/"++FileName),
     Inputs = partition:partition(InputList, Npartitions),
     [spawn(?MODULE, dispatch_work, [Ops, Data, self()]) || Data <- lists:zip(lists:seq(1, Npartitions), Inputs)],
     ResultCollectorPid = spawn(?MODULE, get_results, [self(), #{}, Npartitions]),
-    jobs_queue(Workers, [], ResultCollectorPid).
+    jobs_queue(Workers, [], ResultCollectorPid, FileName).
 
 -spec dispatch_work(Ops, Data, CoordinatorPid) -> ok when
     Ops :: [{Op, Function, integer()}],
@@ -82,8 +84,8 @@ dispatch_work([], Output, CoordinatorPid) ->
     CoordinatorPid ! {endDataflowPartition, Output};
 
 dispatch_work([{reduce, Function, Arg} | Ops], {_, Data}, QueuePid) ->
-    io:format("~n__LOG__ Dispatching work to the queue~n~p", [reduce]),
-    io:format("~n__LOG__ Data: ~w~n", [Data]),
+    ?LOG("~nDispatching work to the queue~n~p", [reduce]),
+    ?LOG("~nData: ~w~n", [Data]),
     QueuePid ! {reduce_prep, Data, self()},
     receive
         {reduce, []} -> not_enough_keys;
@@ -93,7 +95,7 @@ dispatch_work([{reduce, Function, Arg} | Ops], {_, Data}, QueuePid) ->
     end;
 
 dispatch_work([Op | Ops], {PartitionNumber, Data}, QueuePid) ->
-    io:format("__LOG__ Dispatching work to the queue~n~s", [io_lib:format("~p", [Op])]),
+    ?LOG("Dispatching work to the queue~n~s", [io_lib:format("~p", [Op])]),
     QueuePid ! {job, {self(), Op, Data}},
     receive_work(Op, Ops, {PartitionNumber, Data}, QueuePid).
 
@@ -102,38 +104,38 @@ receive_work(Op, Ops, {PartitionNumber, Data}, QueuePid) ->
         {result, Result} ->
             dispatch_work(Ops, {PartitionNumber, Result}, QueuePid);
         {error, Error} ->
-            io:format("Error in coordinator listener ~w~n", [Error]),
+            ?LOG("Error in coordinator listener ~w~n", [Error]),
             dispatch_work([Op | Ops], {PartitionNumber, Data}, QueuePid)
     after 3000 ->
         self() ! {error, "Timeout in coordinator listener"}
     end.
 
-jobs_queue(Workers, DispatchersJobs, ResultCollectorPid)
+jobs_queue(Workers, DispatchersJobs, ResultCollectorPid, FileName)
     when Workers =/= [] andalso DispatchersJobs =/= [] ->
         [Job | Jobs] = DispatchersJobs,
         [Worker | Ls] = Workers,
         gen_tcp:send(Worker, term_to_binary({job, Job})),
-        jobs_queue(Ls, Jobs, ResultCollectorPid);
+        jobs_queue(Ls, Jobs, ResultCollectorPid, FileName);
 
-jobs_queue(Workers, DispatchersJobs, ResultCollectorPid) ->
+jobs_queue(Workers, DispatchersJobs, ResultCollectorPid, FileName) ->
     receive
         {job, Job1} ->
-            jobs_queue(Workers, DispatchersJobs ++ [Job1], ResultCollectorPid); %appends the job
+            jobs_queue(Workers, DispatchersJobs ++ [Job1], ResultCollectorPid, FileName); %appends the job
         {join, NewWorker} ->
-            jobs_queue([NewWorker | Workers], DispatchersJobs, ResultCollectorPid);
+            jobs_queue([NewWorker | Workers], DispatchersJobs, ResultCollectorPid, FileName);
         {reduce_prep, Data, Pid} ->
             ResultCollectorPid ! {reduce_prep, Data, Pid},
-            jobs_queue(Workers, DispatchersJobs, ResultCollectorPid);
+            jobs_queue(Workers, DispatchersJobs, ResultCollectorPid, FileName);
         {endDataflowPartition, Output} ->
             ResultCollectorPid ! {endDataflowPartition, Output},
-            jobs_queue(Workers, DispatchersJobs, ResultCollectorPid);
+            jobs_queue(Workers, DispatchersJobs, ResultCollectorPid, FileName);
         {done_work, Output} ->
             %FIXME: file name 
             file_processing:save_data("data", Output);
         {error, CrushedWorker, Error} ->
-            io:format("Error in coordinator listener ~w~n", [Error]),
+            ?LOG("Error in coordinator listener ~w~n", [Error]),
             Workers1 = lists:delete(CrushedWorker, Workers),
-            jobs_queue(Workers1, DispatchersJobs, ResultCollectorPid)
+            jobs_queue(Workers1, DispatchersJobs, ResultCollectorPid, FileName)
     end.
 
 -spec get_results(CoordinatorPid, OutputMap, N) -> ok when
@@ -144,20 +146,20 @@ jobs_queue(Workers, DispatchersJobs, ResultCollectorPid) ->
 
 get_results(CoordinatorPid, OutputMap, 0) ->
     Output = lists:flatmap(fun({_, V}) -> V end, maps:to_list(OutputMap)),
-    io:format("__LOG__ All results received, sending to the coordinator~n"),
-    io:format("__LOG__ Output: ~w~n", [Output]),
+    ?LOG("All results received, sending to the coordinator~n"),
+    ?LOG("Output: ~w~n", [Output]),
     CoordinatorPid ! {done_work, Output};
 
 get_results(CoordinatorPid, OutputMap, N) ->
     receive
         {endDataflowPartition, {PartitionNumber, Output}} ->
             OutputMap1 = maps:put(PartitionNumber, Output, OutputMap),
-            io:format("__LOG__ Received result from partition ~w~n", [PartitionNumber]),
-            io:format("__LOG__ OutputMap: ~w~n", [OutputMap1]),
+            ?LOG("Received result from partition ~w~n", [PartitionNumber]),
+            ?LOG("OutputMap: ~w~n", [OutputMap1]),
             get_results(CoordinatorPid, OutputMap1, N-1);
         {reduce_prep, Data, DispatcherId} ->
             N1 = prepare_reduce_input([DispatcherId], Data, N, 1),
-            if N =/= N1 -> io:format("__LOG__ Changed number of partitions: ~p became ~p ~n", [N, N1]) end,
+            if N =/= N1 -> ?LOG("Changed number of partitions: ~p became ~p ~n", [N, N1]) end,
             get_results(CoordinatorPid, #{}, N1)
     end.
 
@@ -188,7 +190,7 @@ prepare_reduce_input(DispatchersIds, Data, NPartition, NReceived) when NPartitio
                 #{},
                 Data),
     ListReduce = maps:to_list(MapReduce),
-    io:format("__LOG__ ListReduce: ~w~n", [ListReduce]),
+    ?LOG("ListReduce: ~w~n", [ListReduce]),
     NKey = erlang:length(ListReduce),
     if
         NKey < NPartition ->
@@ -198,7 +200,7 @@ prepare_reduce_input(DispatchersIds, Data, NPartition, NReceived) when NPartitio
     end,
     % Sends each reduced partition to a dispatcher ({{Npartition, Data}, Dispatcher})
     PartitionDataDispatcherList = lists:zip3(lists:seq(1, NPartition), NewPartitionedList, DispatchersIds),
-    io:format("__LOG__ PartitionDataDispatcher: ~w~n", [PartitionDataDispatcherList]),
+    ?LOG("PartitionDataDispatcher: ~w~n", [PartitionDataDispatcherList]),
     [DispatcherId ! {reduce, DataToSend, PartitionNumber} || {PartitionNumber, DataToSend, DispatcherId} <- PartitionDataDispatcherList],
     % The number of partitions can change if the number of keys is less than the number of partitions
     case NKey < NPartition of true -> NKey; false -> NPartition end.
@@ -212,19 +214,23 @@ socket_listener(CoordinatorPid, Sock) ->
         {ok, Msg} ->
             case binary_to_term(Msg) of 
                 join -> 
-                    io:format("New worker has joined~n"),
+                    ?LOG("New worker has joined~n"),
                     CoordinatorPid ! {join, Sock};
                 {result, DispatcherId, Result} ->
                     DispatcherId ! {result, Result},
                     CoordinatorPid ! {join, Sock};
                     % Updates the coordinator about the worker that has finished the job
                 Error ->
-                    io:format("Error in socket listener, unexpected message:~n~w~n", [Error]),
+                    ?LOG("Error in socket listener, unexpected message:~n~w~n", [Error]),
                     CoordinatorPid ! {error, Sock, Error}
             end,
             socket_listener(CoordinatorPid, Sock);
         {error, Error} ->
-            io:format("Error in coordinator listener ~w~n", [Error]),
-            io:format("Closing the socket~n"),
+            ?LOG("Error in coordinator listener ~w~n", [Error]),
+            ?LOG("Closing the socket~n"),
             CoordinatorPid ! {error, Sock, Error}
     end.
+
+get_in(String) ->
+    {ok, In} = io:fread(String, "~s"),
+    In.
